13d68782f1382e8729a3d39b5fb496a4cc9a67c8
cytusalive <cytusomega@gmail.com>
2021-11-16 23:36:50 -0800

    make squares reactive and turn to X on click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button 
+        className="square" 
+        onClick={() => this.setState({value: "X"})}
+      >
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -19,18 +33,18 @@ class Board extends React.Component {
         <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}
+          {this.renderSquare(0)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(0)}
+          {this.renderSquare(0)}
+          {this.renderSquare(0)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(0)}
+          {this.renderSquare(0)}
+          {this.renderSquare(0)}
         </div>
       </div>
     );
